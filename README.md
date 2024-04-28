521f1ab71dfcf1bed52230c06d2016db47fe8cce
Igor Froehner <igoor.sf14@gmail.com>
2024-04-28 17:10:59 -0300

    feat: adds the -o option

--- README.md
+++ README.md
@@ -6,46 +6,35 @@
 stack build
 ```
 
-### Use the executable
+### Usage
 
-#### Do the translation and print it
+```
+stack run -- [--dominance-viz | --graph-viz] <file.ll> [-o <output-file>]
+
+    --graph-viz             outputs a graphviz with the control graph with the block labels
+    --dominance-viz         outputs a graphviz with the dominator tree of the program's control graph
+
+    -o <output-file>        if provided writes the output to the file given
+```
+
+#### Examples
+
+##### Print the translation of the file_path to the terminal
 
 ```
 stack run -- <file_path.ll>
 ```
 
-#### Generate a DOT File `control-graph.dot` for the Control Graph
+##### Generate a DOT File `control-graph.dot` for the Control Graph
 
 ```
-stack run -- --graph-viz <file_path.ll>
+stack run -- --graph-viz <file_path.ll> -o control-graph.dot
 ```
 
-#### Generate a DOT File `dominance.dot` with the Dominance Tree of the Control Graph
+##### Generate a DOT File `dominance.dot` with the Dominance Tree of the Control Graph
 
 ```
-stack run -- --dominance-viz <file_path.ll>
-```
-
-#### Generate a graphviz file to generate a graphical version of the AST
-
-```
-stack run -- --ast <file_path.ll>
-```
-
-### Try in GHCi
-
-```
-stack ghci
-```
-
-#### Some tries on GHCi
-
-```haskell
-runAlex "%1 = call @asdf()" parseLLVMIR
-```
-
-```haskell
-runAlex "call @asdf()" parseLLVMIR
+stack run -- --dominance-viz <file_path.ll> -o dominance.dot
 ```
 
 #### Run Tests
--- app/Main.hs
+++ app/Main.hs
@@ -12,57 +12,46 @@ import System.Environment (getArgs)
 import Data.Graph.Inductive.Graph (Node, lab, suc)
 import Data.Graph.Inductive.PatriciaTree
 import Dominance (buildGraph, dominance)
-import Data.Maybe (fromJust, fromMaybe, listToMaybe)
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        ("--dfs":file:_) -> do
+        ("--graph-viz":file:out) -> do
+            s <- BL.readFile file
+            case runAlex s parseLLVMIR of
+                Left err -> putStrLn err
+                Right ast -> do
+                    let g = buildGraph (head ast)
+                    case out of
+                        ("-o":outFile:_) -> writeFile outFile (plotGraph g)
+                        _ -> putStrLn (plotGraph g)
+        ("--dominance-viz":file:out) -> do
             s <- BL.readFile file
             case runAlex s parseLLVMIR of
                 Left err -> putStrLn err
                 Right ast -> do
                     let g = buildGraph (head ast)
                     let dom = dominance g
-                    mapM_ putStrLn (printTree dom 0)
-        ("--graph-viz":file:_) -> do
+                    case out of
+                        ("-o":outFile:_) -> writeFile outFile (plotGraph dom)
+                        _ -> putStrLn (plotGraph dom)
+        ("--beauty":file:out) -> do
             s <- BL.readFile file
             case runAlex s parseLLVMIR of
                 Left err -> putStrLn err
                 Right ast -> do
-                    let g = buildGraph (head ast)
-                    writeFile "control-graph.dot" (plotGraph g)
-        ("--dominance-viz":file:_) -> do
-            s <- BL.readFile file
-            case runAlex s parseLLVMIR of
-                Left err -> putStrLn err
-                Right ast -> do
-                    let g = buildGraph (head ast)
-                    let dom = dominance g
-                    writeFile "dominance.dot" (plotGraph dom)
-        ("--beauty":file:_) -> do
-            s <- BL.readFile file
-            case runAlex s parseLLVMIR of
-                Left err -> putStrLn err
-                Right ast -> putStrLn (beautyPrint ast)
-                -- Right ast -> writeFile "beauty.out" (beautyPrint ast)
-        (file:_) -> do
+                    case out of
+                        ("-o":outFile:_) -> writeFile outFile (beautyPrint ast)
+                        _ -> putStrLn (beautyPrint ast)
+        (file:out) -> do
             s <- BL.readFile file
             -- print $ runAlex s parseLLVMIR
             case runAlex s parseLLVMIR of
                 Left err -> putStrLn err
                 Right ast -> do
                     let dom = dominance (buildGraph (head ast))
-                    putStrLn (translate ast dom)
-        [] -> putStrLn "Usage: stack run -- <file>"
-
--- Depth-first traversal and formatting
-printTree :: Gr String () -> Node -> [String]
-printTree gr = go 1 where
-  go depth n =
-    let label = fromMaybe "Unknown" (lab gr n)
-        prefix = replicate (depth * 4) ' '  -- Increase the number by 4 spaces for each depth level
-        children = suc gr n
-        childLines = concatMap (go (depth + 1)) children
-    in (prefix ++ label) : childLines
+                    case out of
+                        ("-o":outFile:_) -> writeFile outFile (translate ast dom)
+                        _ -> putStrLn (translate ast dom)
+        [] -> putStrLn "Usage: stack run -- [--dominance-viz | --graph-viz | --beauty] <file.ll> [-o <output-file>]"
