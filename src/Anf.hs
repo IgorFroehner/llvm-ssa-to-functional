60eb30b23a42dd4d64f7730d07d9a1d1a69911cb
Igor Froehner <igoor.sf14@gmail.com>
2024-06-08 13:19:38 -0300

    tests: add one test to translate

--- src/Anf.hs
+++ src/Anf.hs
@@ -16,25 +16,25 @@ module Anf (
   Value(..),
 ) where
 
-newtype Program = Program [Function] deriving Show
+newtype Program = Program [Function] deriving (Show, Eq)
 
 -- put a call to the first block in the end of the function
 data Function =
   Function String [ArgumentDef] Lambda Call
-  deriving Show
+  deriving (Show, Eq)
 
 newtype ArgumentDef =
   ArgumentDef String
-  deriving Show
+  deriving (Show, Eq)
 
 data Lambda =
   Lambda String [ArgumentDef] [Expr] [Lambda] Flow
-  deriving Show
+  deriving (Show, Eq)
 
 newtype Expr
   = ExpDecl Decl
   -- | ExpCall Call
-  deriving Show
+  deriving (Show, Eq)
 
 data Decl
   = DeclBinOp String BinOp
@@ -42,39 +42,39 @@ data Decl
   | DeclIcmp String Icmp
   | DeclSelect String Select
   | DeclConvOp String ConvOp
-  deriving Show
+  deriving (Show, Eq)
 
 newtype ConvOp
   = ConvOp Value
-  deriving Show
+  deriving (Show, Eq)
 
 data Select
   = Select Value Value Value
-  deriving Show
+  deriving (Show, Eq)
 
 data Icmp
   = Icmp String Value Value
-  deriving Show
+  deriving (Show, Eq)
 
 data Flow
   = FlowCall Call
   | FlowCond IfThenElse
-  deriving Show
+  deriving (Show, Eq)
 
 data BinOp
   = BinOp String Value Value
-  deriving Show
+  deriving (Show, Eq)
 
 data Call
   = Call Value [Value]
-  deriving Show
+  deriving (Show, Eq)
 
 data IfThenElse
   = IfThenElse Value Call Call
-  deriving Show
+  deriving (Show, Eq)
 
 data Value
   = Const Integer
   | Name String
   | Unit
-  deriving Show
+  deriving (Show, Eq)
--- src/Ast.hs
+++ src/Ast.hs
@@ -29,26 +29,26 @@ module Ast (
 
 newtype Program a
   = Program [Function a]
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data Function a
   = FunctionDef a (Type a) (Name a) [ArgumentDef a] [BasicBlock a]
   -- | FunctionDec a (Type a) (Name a) [ArgumentDef a]
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data BasicBlock a
   = BasicBlock a (Name a) [PhiDec a] [Stmt a] (Flow a)
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 newtype Stmt a
   = SDec (Dec a)
   -- | SCall (Call a)
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data Flow a
   = FlowBranch (Br a)
   | FlowReturn (Return a)
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data Dec a
   = DecCall a (Name a) (Call a)
@@ -56,82 +56,82 @@ data Dec a
   | DecBinOp a (Name a) (BinOpCall a)
   | DecConvOp a (Name a) (ConvOpCall a)
   | DecSelect a (Name a) (Select a)
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data PhiDec a
   = PhiDec a (Name a) (Phi a)
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data Phi a
   = Phi a (Type a) [(Value a, Name a)]
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data Call a
   = Call a (Type a) (Name a) [CallArgument a]
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data Icmp a
   = Icmp a (Cmp a) (Type a) (Value a) (Value a)
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data BinOpCall a
   = BinOpCall a (BinOp a) (Type a) (Value a) (Value a)
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data ConvOpCall a
   = ConvOpCall a (ConvOp a) (Type a) (Value a) (Type a)
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data Select a
   = Select a (Type a) (Value a) (Value a) (Value a)
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data Return a
   = Return a (Type a) (Maybe (Value a))
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 newtype Branch a
   = Branch (Br a)
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data Name a
   = LName a String
   | GName a String
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data IntegerValue a
   = IntegerValue a Integer
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data Value a
   = ValueInt (IntegerValue a)
   | ValueName (Name a)
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data Type a
   = Type a String
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data CallArgument a
   = CallArgument a (Type a) (Value a)
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data ArgumentDef a
   = ArgumentDef a (Type a) (Maybe (Name a))
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data Cmp a
   = Cmp a String
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data Br a
   = Br a [Name a]
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data BinOp a
   = BinOp a String
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
 
 data ConvOp a
   = ConvOp a String
-  deriving (Foldable, Show)
+  deriving (Foldable, Show, Eq)
